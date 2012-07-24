fbab37d8fde3537f10366ce256ce5c31e551e503
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-23 20:06:48 -0500

    Adicion controlador para el index, y modelo de usuario para usar autenticacion con facebook y google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,39 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :database_authenticatable, :registerable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  validates :email, :presence => true
+  validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+
+  # -- mass-assignment rules --
+  attr_protected :admin, :created_at, :updated_at
+
+  #Be sure to create the users with the info you need.
+  #Note that facebook and gmail will give you different info from users
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.info
+
+    logger.info data.to_yaml
+    
+    if user = User.where(:email => data["email"]).first
+      user.name = data["name"]
+      user.save
+      user
+    else # Create a user with a stub password.
+      User.create!(:email => data["email"], :name => data["name"])
+    end
+  end
+
+  def self.find_for_open_id(access_token, signed_in_resource=nil)
+    data = access_token.info
+
+    logger.info data.to_yaml
+
+    if user = User.where(:email => data["email"]).first
+      user.name = data["name"]
+      user.save
+      user
+    else
+      User.create!(:email => data["email"], :name => data["name"])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NorrisCards::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   root :to => "home#index"
