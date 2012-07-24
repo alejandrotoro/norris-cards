392665e2624b83a925d34600013a8549d2a79260
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-23 19:12:32 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NorrisCards::Application.config.secret_token = '30d232910316c84776641ddc50a0b10734f089d1b50a327cfa5a7c581df4b1cea50cdca33f9dade4d5b36797ec6f4d7717af9227c40f23480be412442af724f1'
