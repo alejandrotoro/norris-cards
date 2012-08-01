f75e7ad100fd6bb2ebf2788e67bf4804cb424faa
Alejandro Toro <alejandrotoro@espiral.co>
2012-07-31 20:00:31 -0500

    quitar la consulta quemada

--- app/controllers/consultas_controller.rb
+++ app/controllers/consultas_controller.rb
@@ -2,7 +2,7 @@ class ConsultasController < ApplicationController
   
   def consultas
   	sql = ActiveRecord::Base.connection()
-  	@consulta = sql.execute("select * from partidas")
+  	@consulta = sql.execute(params[:sql])
     
     respond_to do |format|
       format.html # index.html.erb
